80de83b769b105eaecdcf88770c1040abdc5cdc2
xjm <xu6656863>
2020-10-10 18:43:34 +0800

    回文数优化

--- algo/src/main/java/com/xujm/leetcode/Palindrome.java
+++ algo/src/main/java/com/xujm/leetcode/Palindrome.java
@@ -7,29 +7,30 @@ package com.xujm.leetcode;
  */
 public class Palindrome {
     /**
-     * 时间复杂度O(n)
+     * 反转一半对比
+     * 时间复杂度O(log n)
      * 空间复杂度O(1)
      */
     public static boolean isPalindrome(int x) {
-        try {
-            return x == resverse(x);
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (0 == x) {
+            return true;
+        }
+        if (x < 0 || 0 == x % 10) {
             return false;
         }
-    }
-
-    private static int resverse(int x) {
         int r = 0;
-        while (x > 0) {
+        while (x > r) {
             r = r * 10 + x % 10;
             x = x / 10;
         }
-        return r;
+
+        //偶数||奇数
+        return r == x || x == r / 10;
     }
 
     public static void main(String[] args) {
         System.out.println(isPalindrome(121));
         System.out.println(isPalindrome(1212));
+        System.out.println(isPalindrome(10));
     }
 }
